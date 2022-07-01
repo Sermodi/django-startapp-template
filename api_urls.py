a5c5b161f2c70ace987afc105b22f85f1025ff8f
osuser <sermodi@opal6.opalstack.com>
2022-07-01 08:51:24 +0000

    comillas en app_name, para que sea string

--- api_urls.py
+++ api_urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 #from .views import ...
 
-app_name = {{app_name}}
+app_name = "{{app_name}}"
 
 urlpatterns = [
     # url(r'^$', views.index_view, name='index'),
